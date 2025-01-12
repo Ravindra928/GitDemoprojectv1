17b592eefaf10150e78b5756af7e55f07ce5fe01
Hp <Hp@192.168.1.3>
2025-01-12 19:13:18 +0530

    commiting test 2 to branch x

---
+++ test2.java
@@ -0,0 +1,13 @@
+package iQ;
+
+import reusabiltymethod.Fibonaciseriesmthod;
+
+public class Fainiesseries {
+
+	public static void main(String[] args) {
+		
+		Fibonaciseriesmthod fb=new Fibonaciseriesmthod();
+		fb.fbseries(9);
+	}
+
+}
